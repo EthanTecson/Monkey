6b54eee392691a9a08765058cd9e37de9b9722e1
Ethan Tecson <ethantecson@gmail.com>
2025-02-09 11:55:12 -0500

    first commit

---
+++ Interpreter/token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type		TokenType
+	Litearl		string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF 	= "EOF"
+
+	// Identifers + literals
+	IDENT = "IDENT" // add, foobar, x, y ...
+	INT = "INT" // 123456789
+	
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimeters
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
