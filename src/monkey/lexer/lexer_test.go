a5c14cda79f7b7895fef0ffeb7ec93a9d13f79f3
Ethan Tecson <ethantecson@gmail.com>
2025-02-16 21:26:29 -0500

    finished 1.3. Stopped on page 19.

--- src/monkey/lexer/lexer_test.go
+++ src/monkey/lexer/lexer_test.go
@@ -9,47 +9,47 @@ import (
 	"monkey/token"
 )
 
-func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+// func TestNextToken(t *testing.T) {
+// 	input := `=+(){},;`
 
-	test := []struct {
-		expectedType token.TokenType
-		expectedLiteral string
-	}{
-		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
-		{token.SEMICOLON, ";"},
-		{token.EOF, ""},
-	}
+// 	test := []struct {
+// 		expectedType token.TokenType
+// 		expectedLiteral string
+// 	}{
+// 		{token.ASSIGN, "="},
+// 		{token.PLUS, "+"},
+// 		{token.LPAREN, "("},
+// 		{token.RPAREN, ")"},
+// 		{token.LBRACE, "{"},
+// 		{token.RBRACE, "}"},
+// 		{token.COMMA, ","},
+// 		{token.SEMICOLON, ";"},
+// 		{token.EOF, ""},
+// 	}
 
-	l := New(input)
+// 	l := New(input)
 
-	for i, tt := range test {
-		tok := l.NextToken()
+// 	for i, tt := range test {
+// 		tok := l.NextToken()
 
-		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
-				i, tt.expectedType, tok.Type)
-		}
+// 		if tok.Type != tt.expectedType {
+// 			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+// 				i, tt.expectedType, tok.Type)
+// 		}
 
-		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("test[%d] - literal wrong. expected=%q, got=%q",
-				i, tt.expectedLiteral, tok.Literal)
-		}
-	}
-}
+// 		if tok.Literal != tt.expectedLiteral {
+// 			t.Fatalf("test[%d] - literal wrong. expected=%q, got=%q",
+// 				i, tt.expectedLiteral, tok.Literal)
+// 		}
+// 	}
+// }
 
 func TestNextToken(t *testing.T) {
 	input := `let five = 5;
 let ten = 10;
 
 let add = fn(x,y) {
-x + y
+x + y;
 };
 
 let result = add(five,ten);
